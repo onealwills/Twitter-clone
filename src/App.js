9b0e00c8a293235a75bc3134b21443b1028e1b62
williams <oneal.wills@gmail.com>
2022-02-05 16:05:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import "./Sidebar";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello world!</h1>
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* feed */}
+
+      {/* widgets  */}
+    </div>
+  );
+}
+
+export default App;
